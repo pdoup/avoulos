3ed8679119bf1a12898fb457d3d53236fde9ff00
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2022-02-01 11:05:32 +0200

    Update Kmeans.scala

--- report/5/Kmeans.scala
+++ report/5/Kmeans.scala
@@ -8,6 +8,18 @@ import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
 import org.apache.commons.lang3.StringUtils
 
+// Clustering members or parties using their most represatative keywords
+
+/* 
+   NOTE : This code is meant to run for parties only since the aggregation is done by parties. 
+   	  To alter this so that it can be executed with members, all the instances of political_party
+	  can be changed to member_name.
+*/
+
+// Takes 2 command line parameters
+// n_most_freq : number of most important words to take into account
+// k : number of clusters for kmeans
+
 object Kmeans {
 
   val inputFile = "file:///home/ozzy/Desktop/bd/dtst.csv"
@@ -25,7 +37,8 @@ object Kmeans {
     import ss.implicits._
 
     ss.sparkContext.setLogLevel("WARN")
-
+	
+    // read the csv and convert the date to date format, also take a sample so that it is less computationally intensive
     val df = ss.read.option("header", true)
       .csv(inputFile)
       .select("political_party", "sitting_date", "speech")
@@ -35,15 +48,17 @@ object Kmeans {
       .drop("sitting_date").sample(false, 0.4f, 42L).cache()
 
 
-
+	// strip accents
     	val check = udf((colValue: String) => {StringUtils.stripAccents(colValue)})
 	val df15 = df.select($"political_party",$"date_y", check($"speech").as("speech"))
 
-
+    
+    // group by political_party or member
     val concatSpeechesDF = df15.groupBy("political_party")
       .agg(concat_ws(",", collect_list("speech"))
         .as("speeches"))
 
+    // tokenization
     val speechesDF = new RegexTokenizer().setInputCol("speeches")
       .setOutputCol("speeches_tok")
       .setMinTokenLength(4)
@@ -57,6 +72,7 @@ object Kmeans {
       .toSet
       .toArray
 
+    // removing stopwords
     val speechesFilteredDF = new StopWordsRemover().setCaseSensitive(false)
       .setInputCol("speeches_tok")
       .setOutputCol("speeches_tok1")
@@ -64,9 +80,7 @@ object Kmeans {
       .setStopWords(stopwords)
       .transform(speechesDF)
 
-
-   
-
+    // build a count vectorizer model TF
     val cvModel : CountVectorizerModel = new CountVectorizer().setInputCol("speeches_tok1")
       .setOutputCol("rawFeatures")
       .setMaxDF(0.4f)
@@ -76,7 +90,7 @@ object Kmeans {
     val cvModelDF = cvModel.transform(speechesFilteredDF)
       .drop("speeches", "speeches_tok", "speeches_tok1")
 
-
+    // IDF
     val cvDF = new IDF().setInputCol("rawFeatures")
       .setOutputCol("features")
       .fit(cvModelDF)
@@ -85,6 +99,7 @@ object Kmeans {
 
     val zippedVoc = cvModel.vocabulary.zipWithIndex
 
+    // take the n most frequent keywords per member or per party
     val mostFreq_rdd : RDD[Array[String]]  = cvDF.select("features")
       .rdd
       .map(_.getAs[Vector](0))
@@ -102,7 +117,7 @@ object Kmeans {
         .filter(_.!=()))
       .map(arr => arr.map(_.toString))
 
-
+    
     val membersDF = concatSpeechesDF.select("political_party")
       .rdd
       .map(w => w.toString.replaceAll("[\\[\\]]","").capitalize)
@@ -117,7 +132,7 @@ object Kmeans {
 
     val finalDF = membersDF.join(mostFreqDF, "id").drop("id")
 
-
+    // train a w2vec model on the n most frequent words which will serve as input for the kmeans
     val word2vecDF = new Word2Vec().setMaxSentenceLength(n_most_freq)
       .setMinCount(0)
       .setInputCol("Most_Frequent_TFIDF")
@@ -126,6 +141,8 @@ object Kmeans {
       .fit(finalDF)
       .transform(finalDF)
 
+    // K-Means to find meaningful clusters using w2vec representations
+    // k is a command line parameter
     val kmeansDF = new KMeans().setFeaturesCol("embeddings")
       .setSeed(42)
       .setK(k)
@@ -137,14 +154,14 @@ object Kmeans {
     println("=============================================")
     println("Evaluating the clustering w/ silhouette score...")
 
-
+    // evaluate the clustering w/ silhouette
     val silhouette = new ClusteringEvaluator().setFeaturesCol("embeddings").evaluate(kmeansDF)
     println(f"score : $silhouette%.7f")
 
     println("=============================================")
 
     kmeansDF.groupBy("prediction").count().show(false)
-
+    // write to file the name of the member/party and the cluster in belongs to
     kmeansDF.select("name", "prediction").write.option("header", true).csv(s"file:///home/ozzy/Desktop/bd/Erwthma5/kmeans_out_k_party${k}")
 
     println("Output written to csv file")
